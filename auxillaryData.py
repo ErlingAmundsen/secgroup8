543a7192f0451ab98c74732921a3a90e04825db7
Sebastian <andersen_sebastian@yahoo.dk>
2023-11-13 14:19:50 +0100

    Create auxillaryData.py

---
+++ auxillaryData.py
@@ -0,0 +1,13 @@
+import pandas as pd
+def main():
+    # Load the original datasets
+    file_path = 'GroupG/private_dataG.xlsx'
+    data_private = pd.read_excel(file_path)
+
+    # Create auxillary data
+    auxillary_data = data_private[["name"]]
+
+    # Save auxillary data
+    auxillary_data.to_csv("GroupG/auxillary_dataG.csv", index=False)
+if __name__ == "__main__":
+    main()
