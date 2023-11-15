2c11712ae7ff48a73b6630e85ce6c7e58cbbd939
Alexander Winther Nielsen <alwn@itu.dk>
2023-11-15 20:52:38 +0100

    Style: change of code style, expanding lists to multiline

--- anonymisation.py
+++ anonymisation.py
@@ -1,69 +1,115 @@
-import pandas as pd
 from datetime import datetime
 
+import pandas as pd
+
 # 2. Generalize Date of Birth to Age Groups
+
+
 def bin_age(data_private):
-        current_year = datetime.now().year
-        data_private['dob'] = data_private['dob'].apply(lambda x: current_year - x.year)  # Converting DOB to age
-        # data_private['dob'] = data_private['dob'].apply(lambda x: x - x % 10)  # Grouping ages into 10 year groups
-        groups = {range(18,26): '18-25', range(26,46): '26-45',range(46,61): '46-60',range(61,200): '61+' }
-        data_private.replace({'dob': groups}, inplace=True)
-        return data_private
+    current_year = datetime.now().year
+    data_private["dob"] = data_private["dob"].apply(
+        lambda x: current_year - x.year
+    )  # Converting DOB to age
+    # data_private['dob'] = data_private['dob'].apply(lambda x: x - x % 10)  # Grouping ages into 10 year groups
+    groups = {
+        range(18, 26): "18-25",
+        range(26, 46): "26-45",
+        range(46, 61): "46-60",
+        range(61, 200): "61+",
+    }
+    data_private.replace({"dob": groups}, inplace=True)
+    return data_private
+
 
 # 3. Transform Education
+
+
 def bin_education(data_private):
-        groups = {'Not stated': 'Primary education',  
-        'Bachelors programmes': 'University degree',
-        'Masters programmes': 'University degree',
-        'PhD programmes': 'University degree',
-        'Short cycle higher education': 'University degree'}
-        data_private.replace({'education': groups}, inplace=True)
-        return data_private
+    groups = {
+        "Not stated": "Primary education",
+        "Bachelors programmes": "University degree",
+        "Masters programmes": "University degree",
+        "PhD programmes": "University degree",
+        "Short cycle higher education": "University degree",
+    }
+    data_private.replace({"education": groups}, inplace=True)
+    return data_private
+
 
 # 4. Change Nationality to other
+
+
 def bin_citezenship(data_private):
-        data_private['citizenship'] = data_private['citizenship'].apply(lambda x: 'Other' if x != 'Denmark' else x)
-        return data_private
+    data_private["citizenship"] = data_private["citizenship"].apply(
+        lambda x: "Other" if x != "Denmark" else x
+    )
+    return data_private
 
-def anonymize(data_private:pd.DataFrame):
 
-        # Analyzing the original k-anonymity
-        if 'education' in data_private.columns:
-                original_re_identification_columns = ['name', 'sex', 'dob', 'zip', 'education', 'citizenship', 'marital_status', 'party']
+def anonymize(data_private: pd.DataFrame):
+    # Analyzing the original k-anonymity
+    if "education" in data_private.columns:
+        original_re_identification_columns = [
+            "name",
+            "sex",
+            "dob",
+            "zip",
+            "education",
+            "citizenship",
+            "marital_status",
+            "party",
+        ]
+    else:
+        if "party" in data_private.columns:
+            original_re_identification_columns = [
+                "name",
+                "sex",
+                "dob",
+                "zip",
+                "citizenship",
+                "marital_status",
+                "party",
+            ]
         else:
-                if 'party' in data_private.columns:
-                        original_re_identification_columns = ['name', 'sex', 'dob', 'zip', 'citizenship', 'marital_status', 'party']
-                else:
-                        original_re_identification_columns = ['name', 'sex', 'dob', 'zip', 'citizenship', 'marital_status']
+            original_re_identification_columns = [
+                "name",
+                "sex",
+                "dob",
+                "zip",
+                "citizenship",
+                "marital_status",
+            ]
 
+    original_k_anonymity_groups = data_private.groupby(
+        original_re_identification_columns
+    ).size()
+    original_k_anonymity = original_k_anonymity_groups.min()
 
-        original_k_anonymity_groups = data_private.groupby(original_re_identification_columns).size()
-        original_k_anonymity = original_k_anonymity_groups.min()
+    # remove name
+    data_private.drop("name", axis=1, inplace=True)
+    # remove zip
+    data_private.drop("zip", axis=1, inplace=True)
+    # remove marital_status
+    data_private.drop("marital_status", axis=1, inplace=True)
 
-        # remove name
-        data_private.drop('name', axis=1, inplace=True)
-        # remove zip
-        data_private.drop('zip', axis=1, inplace=True)
-        # remove marital_status
-        data_private.drop('marital_status', axis=1, inplace=True)
-        
-        # remove citizenship
-        data_private.drop('citizenship', axis=1, inplace=True)
-        
-        # 2. Generalize Date of Birth to Age Groups
-        data_private = bin_age(data_private)
+    # remove citizenship
+    data_private.drop("citizenship", axis=1, inplace=True)
 
-        # 3. Transform Education if exists
-        if 'education' in data_private.columns:
-                data_private = bin_education(data_private)
-        return data_private
+    # 2. Generalize Date of Birth to Age Groups
+    data_private = bin_age(data_private)
+
+    # 3. Transform Education if exists
+    if "education" in data_private.columns:
+        data_private = bin_education(data_private)
+    return data_private
+
 
 def main():
-        data_frame = pd.read_excel('GroupG/private_dataG.xlsx')
-        data_private = anonymize(data_frame)
-        print(data_private['education'].value_counts())
-        print(data_private['evote'].sum())
-        
+    data_frame = pd.read_excel("GroupG/private_dataG.xlsx")
+    data_private = anonymize(data_frame)
+    print(data_private["education"].value_counts())
+    print(data_private["evote"].sum())
+
 
 if __name__ == "__main__":
-        main()
+    main()
